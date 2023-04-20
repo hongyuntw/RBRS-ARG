cc6aad41235c802ede784cd3ef94320ff895e3f8
hung-yun chiang <harry.chiang@appier.com>
2023-04-20 16:33:45 +0800

    update utils.py

--- utils.py
+++ utils.py
@@ -56,86 +56,45 @@ def collate_fn(batch):
     return data, label
 
 
-def review_pos_in_doc(doc, review_ids):
-    # review_ids = review_ids[:-1]
-    for i in range(len(doc)):
-        count = 0
-        if doc[i] == review_ids[count]:
-            k = i 
-            while k < len(doc) and doc[k] == review_ids[count]:
-                count += 1
-                k += 1
-                if count == len(review_ids):
-                    return i, i + len(review_ids)
-            
-    return -1, -1
-
-def unpack_input(opt, x, ranking=False):
-    if not ranking:
-        uids, iids = list(zip(*x))
-        uids = list(uids)
-        iids = list(iids)
+def unpack_input(opt, x):
+    uids_batch , iids_batch = x
+    uids_batch = list(uids_batch)
+    iids_batch = list(iids_batch)
 
 
-        # b_size, #reviews, review_max_length
+    user_reviews_batch = []
+    user_item2id_batch = []
+    user_doc_batch = []
+    item_reviews_batch = []
+    item_user2id_batch = []
+    item_doc_batch = []
+
+    for i in range(len(uids_batch)):
+        uids = uids_batch[0]
+        iids = iids_batch[0]
+        
+
+        # num_items * #reviews, review_max_length
         user_reviews = opt.users_review_list[uids] 
-
-        # b_size, #reviews
-        user_item2id = opt.user2itemid_list[uids]  # 检索出该user对应的item id
-
-        # b_size, doc_max_length
+        # num_items * #reviews
+        user_item2id = opt.user2itemid_list[uids]  
+        # num_items * doc_max_length
         user_doc = opt.user_doc[uids]
 
         item_reviews = opt.items_review_list[iids]
-        item_user2id = opt.item2userid_list[iids]  # 检索出该item对应的user id
+        item_user2id = opt.item2userid_list[iids]  
         item_doc = opt.item_doc[iids]
 
-        data = [user_reviews, item_reviews, uids, iids, user_item2id, item_user2id, user_doc, item_doc]
-        data = list(map(lambda x: torch.LongTensor(x).cuda(), data))
-        return data
-    
-    else:
-        
-        uids_batch , iids_batch = x
-        uids_batch = list(uids_batch)
-        iids_batch = list(iids_batch)
+        user_reviews_batch.append(user_reviews)
+        user_item2id_batch.append(user_item2id)
+        user_doc_batch.append(user_doc)
+        item_reviews_batch.append(item_reviews)
+        item_user2id_batch.append(item_user2id)
+        item_doc_batch.append(item_doc)
 
 
-        user_reviews_batch = []
-        user_item2id_batch = []
-        user_doc_batch = []
-        item_reviews_batch = []
-        item_user2id_batch = []
-        item_doc_batch = []
-
-        for i in range(len(uids_batch)):
-            uids = uids_batch[0]
-            iids = iids_batch[0]
-            
-
-            # num_item when training = 5 (1 pos, 4 neg)
-
-            # num_items * #reviews, review_max_length
-            user_reviews = opt.users_review_list[uids] 
-            # num_items * #reviews
-            user_item2id = opt.user2itemid_list[uids]  # 检索出该user对应的item id
-            # num_items * doc_max_length
-            user_doc = opt.user_doc[uids]
-
-            item_reviews = opt.items_review_list[iids]
-            item_user2id = opt.item2userid_list[iids]  # 检索出该item对应的user id
-            item_doc = opt.item_doc[iids]
-
-            user_reviews_batch.append(user_reviews)
-            user_item2id_batch.append(user_item2id)
-            user_doc_batch.append(user_doc)
-            item_reviews_batch.append(item_reviews)
-            item_user2id_batch.append(item_user2id)
-            item_doc_batch.append(item_doc)
-
-
-        data = [user_reviews_batch, item_reviews_batch, uids_batch, iids_batch, user_item2id_batch, item_user2id_batch, user_doc_batch, item_doc_batch]
-        data = list(map(lambda x: torch.LongTensor(x).cuda(), data))
-        return data
+    data = [user_reviews_batch, item_reviews_batch, uids_batch, iids_batch, user_item2id_batch, item_user2id_batch, user_doc_batch, item_doc_batch]
+    data = list(map(lambda x: torch.LongTensor(x).cuda(), data))
+    return data
 
 
